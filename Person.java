0eafa32f44c4163cbd011b30be8ccc60231c043f
Prakashkrmkp <prakashkrmkp@gmail.com>
2016-08-30 09:55:49 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,34 @@
+package com.prakash.persons;
+
+public class Person {
+	
+	String name;
+	String address;
+	public Person() {
+		super();
+		
+	}
+	public Person(String name, String address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String toString() {
+		return "Person [name=" + name + ", address=" + address + "]";
+	}
+	
+
+}
